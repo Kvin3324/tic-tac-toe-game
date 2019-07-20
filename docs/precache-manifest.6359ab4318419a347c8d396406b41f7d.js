7488864f3f081b706cde87a2878133ee93b2b0ce
Kévin Joya <kevin.joya@outlook.com>
2019-07-20 15:16:36 +0200

    Added build folder docs

---
+++ docs/precache-manifest.6359ab4318419a347c8d396406b41f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c2772d1cc3b723c98a560c160acf36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faa5732d41677c5a1bc5",
+    "url": "/static/css/main.9151d181.chunk.css"
+  },
+  {
+    "revision": "7b60294cd21fadfbbb6e",
+    "url": "/static/js/2.4a004fb4.chunk.js"
+  },
+  {
+    "revision": "faa5732d41677c5a1bc5",
+    "url": "/static/js/main.a7fdce92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
